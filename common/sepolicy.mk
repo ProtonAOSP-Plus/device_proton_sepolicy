35ac9aaf7965fd096a917fcb1d1f25ca91821fb6
Md Abu Talha <rokusenpai06@gmail.com>
2022-02-17 10:13:49 +0000

    proton-sepolicy: adapt to proton

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,6 +1,6 @@
 #
 # This policy configuration will be used by all products that
-# inherit from 404
+# inherit from proton
 #
 
 ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
@@ -10,20 +10,20 @@ endif
 endif
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    device/404/sepolicy/common/public
+    device/proton/sepolicy/common/public
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/404/sepolicy/common/private
+    device/proton/sepolicy/common/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/404/sepolicy/common/dynamic \
-    device/404/sepolicy/common/system-only
+    device/proton/sepolicy/common/dynamic \
+    device/proton/sepolicy/common/system-only
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/404/sepolicy/common/dynamic \
-    device/404/sepolicy/common/vendor
+    device/proton/sepolicy/common/dynamic \
+    device/proton/sepolicy/common/vendor
 endif
 
 # Selectively include legacy rules defined by the products
--include device/404/sepolicy/legacy-common/sepolicy.mk
+-include device/proton/sepolicy/legacy-common/sepolicy.mk
--- legacy-common/sepolicy.mk
+++ legacy-common/sepolicy.mk
@@ -1,9 +1,9 @@
 #
 # This policy configuration will be used by select legacy products that
-# inherit from 404
+# inherit from proton
 #
 
 ifeq ($(TARGET_HAS_LEGACY_CAMERA_HAL1), true)
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    device/404/sepolicy/legacy-common/public/legacy-camera-hal1
+    device/proton/sepolicy/legacy-common/public/legacy-camera-hal1
 endif
